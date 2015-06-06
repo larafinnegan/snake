2abf9443bd220a84d96c6488ae635e8d2d2b7f7a
Lara Finnegan <lcfinnegan@gmail.com>
2015-06-06 15:10:14 -0500

    fixed the clear interval function and added conditions to the move and die methods

--- jasmine/src/snake.js
+++ jasmine/src/snake.js
@@ -28,12 +28,12 @@ var food = {
 	location: 0,
 	
 	create: function() {
-		var i = Math.floor(Math.random() * 40);
-		var j = Math.floor(Math.random() * 40);
+		var i = Math.floor(Math.random() * board.area.length);
+		var j = Math.floor(Math.random() * board.area.length);
 		
 		while (board.area[i][j]) {
-			i = Math.floor(Math.random() * 40);
-			j = Math.floor(Math.random() * 40);
+			i = Math.floor(Math.random() * board.area.length);
+			j = Math.floor(Math.random() * board.area.length);
 		}
 	$('#' + i + "-" + j).addClass('food');
 	this.location = [i, j];
@@ -50,12 +50,19 @@ var snake = {
 	direction: "r",
 	
 	changeDirection: function(event) {
-		switch(event.which) {
-			case 37: return this.direction = "l";
-			case 38: return this.direction = "u";
-			case 39: return this.direction = "r";
-			case 40: return this.direction = "d";
-			default: return this.direction;
+		if (this.direction === "r" || this.direction === "l") {
+			switch(event.which) {
+				case 40: return this.direction = "d";
+				case 38: return this.direction = "u";
+				default: return this.direction;
+			}
+		}
+		else {
+			switch(event.which) {
+				case 39: return this.direction = "r";
+				case 37: return this.direction = "l";
+				default: return this.direction;
+			}
 		}
 	},
 
@@ -89,8 +96,11 @@ var snake = {
 	},
 	
 	die: function() {
-		if (this.body[0][0] < 0 || this.body[0][0] > 39) return true;
-		if (this.body[0][1] < 0 || this.body[0][1] > 39) return true;
+		var x = this.body[0][0];
+		var y = this.body[0][1];
+		if ((x < 0 || x > board.area.length - 1) ||
+		(y < 0 || y > board.area.length - 1) ||
+		(board.area[x][y] === "X")) return true;
 	}
 };
 
@@ -100,15 +110,19 @@ $(document).ready(function() {
 });
 
 var game = {
-
-	intervalId: 0,
+	
+	whileInterval: function(proc, int) {
+		var timerID = setInterval(function() {
+		if (proc()) clearInterval(timerID);
+		}, int);
+	},
 	
 	init: function() {
 		board.createArea();
 		$("#board").append(board.render());
 		$("#20-20").addClass('snake');
 		food.create();
-		setInterval(this.play, 500);
+		this.whileInterval(this.play, 200);
 	},
 	
 	play: function() {
@@ -116,19 +130,15 @@ var game = {
 			snake.changeDirection(event);
 		})
 		snake.move();
-		if (snake.die()) {
-			clearInterval(1);
-			console.log("hello");
+		if (snake.die()) return true;
+		if (snake.eat()) {
+			food.remove();
+			food.create();
 		}
 		else {
-			if (snake.eat()) {
-				food.remove();
-				food.create();
-			}
-			else {
-				snake.updateTail();
-			}
-			snake.updateHead();
+			snake.updateTail();
 		}
+		snake.updateHead();
+		console.log("loop");
 	}
 };
